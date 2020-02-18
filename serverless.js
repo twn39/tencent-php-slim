6d43b78523451aca005773f8ce85483092ddaf53
yugasun <yuga_sun@163.com>
2020-02-18 17:55:46 +0800

    feat: update dependency apigw for supporting custom domain

--- serverless.js
+++ serverless.js
@@ -8,6 +8,13 @@ const path = require('path');
 const { Component, utils } = require('@serverless/core');
 
 module.exports = class TencentPHPSlim extends Component {
+    getDefaultProtocol(protocols) {
+        if (protocols.map((i) => i.toLowerCase()).includes('https')) {
+          return 'https'
+        }
+        return 'http'
+    }
+
     async default(inputs = {}) {
         inputs.name =
             ensureString(inputs.functionName, { isOptional: true }) ||
@@ -34,7 +41,6 @@ module.exports = class TencentPHPSlim extends Component {
         inputs.runtime = 'Php7';
 
         const tencentCloudFunction = await this.load('@serverless/tencent-scf');
-        const tencentApiGateway = await this.load('@serverless/tencent-apigateway');
 
         if (inputs.functionConf) {
             inputs.timeout = inputs.functionConf.timeout || 3;
@@ -45,37 +51,53 @@ module.exports = class TencentPHPSlim extends Component {
 
         inputs.fromClientRemark = inputs.fromClientRemark || 'tencent-php-slim';
         const tencentCloudFunctionOutputs = await tencentCloudFunction(inputs);
-        const apigwParam = {
-            serviceName: inputs.serviceName,
-            description: 'Serverless Framework tencent-php-slim Component',
-            serviceId: inputs.serviceId,
-            region: inputs.region,
-            protocol: apigatewayConf.protocol || 'http',
-            environment: apigatewayConf.environment || 'release',
-            endpoints: [
-                {
-                    path: '/',
-                    method: 'ANY',
-                    function: {
-                        isIntegratedResponse: true,
-                        functionName: tencentCloudFunctionOutputs.Name,
-                    },
-                },
-            ],
-        };
-        if (apigatewayConf.usagePlan) apigwParam.endpoints[0].usagePlan = apigatewayConf.usagePlan;
-        if (apigatewayConf.auth) apigwParam.endpoints[0].auth = inputs.apigatewayConf.auth;
 
+        const outputs = {
+            region: inputs.region,
+            functionName: inputs.name,
+        };
+
+        // if not disable, then create apigateway
+        if (!apigatewayConf.isDisabled) {
+            const tencentApiGateway = await this.load('@serverless/tencent-apigateway');
+            const apigwParam = {
+                serviceName: inputs.serviceName,
+                description: 'Serverless Framework tencent-php-slim Component',
+                serviceId: inputs.serviceId,
+                region: inputs.region,
+                protocols: apigatewayConf.protocols || ['http'],
+                environment: apigatewayConf.environment || 'release',
+                endpoints: [
+                    {
+                        path: '/',
+                        method: 'ANY',
+                        function: {
+                            isIntegratedResponse: true,
+                            functionName: tencentCloudFunctionOutputs.Name,
+                        },
+                    },
+                ],
+                customDomain: apigatewayConf.customDomain,
+            };
+            if (apigatewayConf.usagePlan) apigwParam.endpoints[0].usagePlan = apigatewayConf.usagePlan;
+            if (apigatewayConf.auth) apigwParam.endpoints[0].auth = inputs.apigatewayConf.auth;
+    
+            apigwParam.fromClientRemark = inputs.fromClientRemark || 'tencent-php-slim';
+            const tencentApiGatewayOutputs = await tencentApiGateway(apigwParam);
+            outputs.apiGatewayServiceId = tencentApiGatewayOutputs.serviceId;
+            outputs.url = `${this.getDefaultProtocol(tencentApiGatewayOutputs.protocols)}://${
+                tencentApiGatewayOutputs.subDomain
+            }/${tencentApiGatewayOutputs.environment}/`;
+
+            if (tencentApiGatewayOutputs.customDomains) {
+                outputs.customDomains = tencentApiGatewayOutputs.customDomains;
+            }
+        }
+        
         this.state.functionName = inputs.name;
         await this.save();
-        apigwParam.fromClientRemark = inputs.fromClientRemark || 'tencent-php-slim';
-        const tencentApiGatewayOutputs = await tencentApiGateway(apigwParam);
-        return {
-            region: tencentApiGatewayOutputs.region,
-            functionName: inputs.name,
-            apiGatewayServiceId: tencentApiGatewayOutputs.serviceId,
-            url: `${tencentApiGatewayOutputs.protocols[0]}://${tencentApiGatewayOutputs.subDomain}/${tencentApiGatewayOutputs.environment}/`,
-        };
+        
+        return outputs;
     }
 
     async remove(inputs = {}) {
